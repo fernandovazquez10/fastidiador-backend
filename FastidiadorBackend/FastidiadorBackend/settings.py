3942da6063726f0891c0e1055fa0c1c3f6ea4e19
Alonso <f.vazquez151000@gmail.com>
2022-09-27 00:50:56 -0500

    Fix

--- FastidiadorBackend/FastidiadorBackend/settings.py
+++ FastidiadorBackend/FastidiadorBackend/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--gvk*dm3-#e(iwmewl7liav-x1ge*niv(k&2!7!p*-m9^0n_p3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
